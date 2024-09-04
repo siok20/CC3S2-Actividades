a26befe7d1e002dfa493656b0249de318038f445
Eladio <eladiohv2@gmail.com>
2024-09-04 13:11:38 -0500

    Agregado de la Actividad 2

---
+++ Actividad-2/devops-practice/src/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello, World!');
+});
+
+
+module.exports = app;
+
+
+if (require.main === module) {
+    const port = process.env.PORT || 3000; 
+    app.listen(port, () => {
+        console.log(`Server running on port ${port}`);
+    });
+}
