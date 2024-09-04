e297729a3d64e0fd114d56c24696cf88eeda3852
Eladio <eladiohv2@gmail.com>
2024-09-04 08:04:56 -0500

    Initial commit

---
+++ Actividad-1/devops-practice/tests/app.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const app = require('../src/app');
+
+
+describe('GET/', ()=>{
+    let server;
+
+    beforeAll('GET /', () => {
+        server = app.listen(0);
+    });
+
+    afterAll(() => {
+        server.close();
+    });
+
+    it('should return Hello, World!', async()=>{
+        const res = await request(app).get('/');
+        expect(res.statusCode).toEqual(200);
+        expect(res.text).toBe('Hello, World!');
+    });
+});
